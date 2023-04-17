07887604518771841bedef1db5f6741e2a0790ad
Qunlif <130246868+Qunlif@users.noreply.github.com>
2023-04-17 20:12:19 -0300

    Alright, This is the first verison, hope it commits properly.

---
+++ src/main/java/net/qunlif/thermalkill/ThermalKillClient.java
@@ -0,0 +1,10 @@
+package net.qunlif.thermalkill;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ThermalKillClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/qunlif/thermalkill/ThermalKillDataGenerator.java
@@ -0,0 +1,11 @@
+package net.qunlif.thermalkill;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ThermalKillDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
